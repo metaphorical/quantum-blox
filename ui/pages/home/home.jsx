871f08e1908cfcf731f58e3e27ab58572f9e1d24
Rastko Vukašinović <rastko.vukasinovic@gmail.com>
2017-09-18 12:05:18 +0200

    fixed build failing

--- ui/pages/home/home.jsx
+++ ui/pages/home/home.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 
 import DefaultLayout from '../../layouts/default';
 
-import BadgesComponent from '../../components/badges';
 
 export default function() {
     return (
         <DefaultLayout >
             <h1>Hi there</h1>
             <p>
-                Welcome to my quickest protype, production ready stack. This page is made to share my take on modern UI development
+                Welcome to quantum blocks
             </p>
-            <p>
-                If you use this, you'll have good separation of concerns, good and well isolated components, no more wierd css problems and nice development experience.
-            </p>
-            <p>
-                Downside is - you'll have to understand a bit of functional programming, a lot about modern javascript techniques and learn some stuff. Namely:
-            </p>
-            <BadgesComponent />
             
         </DefaultLayout>
     );
